b21fa0e0e6719f368c737d2b10b71d55e33394cc
asman255 <smandth@gmail.com>
2025-02-25 18:09:51 +0700

    mostly done

--- server/routes/tester.js
+++ server/routes/tester.js
@@ -2,6 +2,23 @@ const express = require("express");
 const prisma = require("../config/prisma");
 const router = express.Router();
 
+router.get("/today-pass", async (req, res) => {
+  try {
+    const today = new Date().toISOString().split("T")[0];
+    const testers = await prisma.tester.findMany({
+      where: {
+        updatedAt: { gt: new Date(`${today}T00:00:00.000Z`) },
+      },
+    });
+    const passCount = testers.filter(tester => tester.final === "Pass").length;
+    const notPassCount = testers.filter(tester => tester.final === "Not Pass").length;
+    res.json({ pass: passCount, notpass: notPassCount });
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching testers" });
+    console.log(error);
+  }
+});
+
 router.get("/", async (req, res) => {
   try {
     const testers = await prisma.tester.findMany();
@@ -10,7 +27,6 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Error fetching testers" });
   }
 });
-
 
 router.get("/:id", async (req, res) => {
   try {
@@ -43,14 +59,13 @@ router.post("/", async (req, res) => {
       giveway,
       practice,
     } = req.body;
-    console.log(req.body)
-    
+    console.log(req.body);
 
     const bodyScore = colorblind + longsight + astigmatic + reflec;
     const bodyresult = bodyScore >= 30 ? true : false;
     const theoryScore = sign + line + giveway;
     const theoryresult = theoryScore / 150 >= 0.8 ? true : false;
-    const final = bodyresult && theoryresult && practice ? "true" : "false";
+    const final = bodyresult && theoryresult && practice ? "Pass" : "Not Pass";
 
     await prisma.tester.upsert({
       where: { id },
@@ -91,55 +106,9 @@ router.post("/", async (req, res) => {
     res.status(201).json({ message: "Data inserted successfully" });
   } catch (error) {
     res.status(500).json({ error: "Error inserting data" });
-    console.log(error)
+    console.log(error);
   }
 });
-
-
-
-
-
-
-// router.put("/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const {
-//       name,
-//       lastname,
-//       colorblind,
-//       longsight,
-//       astigmatic,
-//       reflec,
-//       sign,
-//       line,
-//       giveway,
-//       practice,
-//     } = req.body;
-
-//     const bodyScore = colorblind + longsight + astigmatic + reflec;
-//     const bodyresult = bodyScore >= 30 ? true : false;
-//     const theoryScore = sign + line + giveway;
-//     const theoryresult = theoryScore / 150 >= 0.8 ? true : false;
-//     const final = bodyresult && theoryresult && practice ? "true" : "false";
-
-//     await prisma.tester.update({
-//       where: { id },
-//       data: {
-//         name,
-//         lastname,
-//         bodyresult,
-//         theoryresult,
-//         final,
-//         practice,
-//       },
-//     });
-
-//     res.status(200).json({ message: "Data updated successfully" });
-//   } catch (error) {
-//     res.status(500).json({ error: "Error updating data" });
-//     console.log(error);
-//   }
-// });
 
 router.delete("/:id", async (req, res) => {
   try {
